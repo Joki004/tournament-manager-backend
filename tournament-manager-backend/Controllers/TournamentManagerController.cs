aebe0663bc9c9c23f9864643eecc69c4ceb9747b
Joram Mumb <jorammumb15.jm@gmail.com>
2024-05-22 16:52:44 +0200

    Add project files.

---
+++ tournament-manager-backend/Controllers/TournamentManagerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace tournament_manager_backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TournamentManagerController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Tournaments")]
+        public IActionResult GetTournaments()
+        {
+            try
+            {
+                string query = "select * from dbo.TOURNAMENTS";
+                DataTable table = new DataTable();
+                string sqlDatasource = Environment.GetEnvironmentVariable("TournamentAppDBCon") ?? throw new InvalidOperationException("Connection string 'TournamentAppDBCon' is not configured.");
+                Console.WriteLine($"Connecting to: {sqlDatasource}");
+
+                using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        using (SqlDataReader myReader = myCommand.ExecuteReader())
+                        {
+                            table.Load(myReader);
+                        }
+                    }
+                }
+
+                return new JsonResult(table);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception in GetTournaments: {ex}");
+                throw;
+            }
+        }
+    }
+}
